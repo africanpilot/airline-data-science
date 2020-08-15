da6794db3deb8bb49919a2cc5b972ec453247dde
africanpilot <makurichard14@gmail.com>
2020-08-15 15:04:56 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://africanpilot.github.io/airline-data-science/precache-manifest.9bf22888414b49542df29a127f6a7bf1.js"
+  "https://africanpilot.github.io/airline-data-science/precache-manifest.0087cf58cea5163e7028079a89ab2415.js"
 );
 
 self.addEventListener('message', (event) => {
